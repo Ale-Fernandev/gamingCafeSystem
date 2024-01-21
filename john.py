88e050044fb14f4d0200f5b6473945ab8c74c5de
Ale Fernandez <AleFern99007@gmail.com>
2024-01-20 19:11:13 -0500

    First Version of gamingCafeSystem

---
+++ john.py
@@ -0,0 +1,8 @@
+print("This will always be ran")
+
+
+def main():
+    print("First Module's Main Method")
+
+    if __name__ == "__main__":
+        main()
---
+++ test.py
@@ -0,0 +1,6 @@
+import john
+
+john.main()
+
+
+print(f'Test modules name is {__name__}')
